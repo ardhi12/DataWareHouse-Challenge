574fb64e42d55ed392a0384d1bde693daadb0563
ardhi <ardhiwahyudhi12@gmail.com>
2021-04-24 12:52:16 +0700

    visualize denormalized table

--- solution/main.py
+++ solution/main.py
@@ -40,10 +40,25 @@ def complete_historical():
         df_list.append(df_dir)
     return df_list
 
+def denormalized_table(historical_each_table):
+    """
+    This function is used to denormalize joined table
+    """
+    # joining tables using concat, sort values by timestamp and DB operation
+    # and set column 'id' as index
+    data_frames = pd.concat(historical_each_table, axis=0).sort_values(by=['ts','op']).set_index('id')
+    # display the visualization
+    print(f"Visualization of the denormalized joined table")        
+    display(data_frames)
+    print("")
+    return data_frames
+
 def main():
     # Visualize the complete historical table view of each tables
     historical_each_tables = complete_historical()
     
+    # Visualize the complete historical table view of the denormalized joined table
+    denormalized_table(historical_each_tables)    
 
 if __name__ == "__main__":
     main()
